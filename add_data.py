71133e5039927032011a2b7e285320dc06bff0c8
rienafairefr <rienafairefr@gmail.com>
2017-03-06 15:58:35 +0100

    reformat file

--- add_data.py
+++ add_data.py
@@ -12,30 +12,28 @@ from rebrickable_db.models.parts import Part
 from rebrickable_db.models.sets import Set
 from rebrickable_db.models.themes import Theme
 
-
 Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 session = Session()
 
-data={
-'colors.csv':Color,
-'inventories.csv':Inventory,
-'inventory_parts.csv':InventoryPart,
-'inventory_sets.csv':InventorySet,
-'part_categories.csv':PartCategory,
-'parts.csv':Part,
-'sets.csv':Set,
-'themes.csv':Theme,
+data = {
+    'colors.csv': Color,
+    'inventories.csv': Inventory,
+    'inventory_parts.csv': InventoryPart,
+    'inventory_sets.csv': InventorySet,
+    'part_categories.csv': PartCategory,
+    'parts.csv': Part,
+    'sets.csv': Set,
+    'themes.csv': Theme,
 }
 
-
-for filename,typ in data.items():
-    print('Treating %s ...'%filename)
-    csv_reader = reader(open(os.path.join('rebrickable_data', filename),encoding='utf-8'))
+for filename, typ in data.items():
+    print('Treating %s ...' % filename)
+    csv_reader = reader(open(os.path.join('rebrickable_data', filename), encoding='utf-8'))
     headers = next(csv_reader, None)
-    dictionaries=[]
+    dictionaries = []
     for row in csv_reader:
-        dictionary = {headers[i]:row[i] for i in range(len(headers))}
+        dictionary = {headers[i]: row[i] for i in range(len(headers))}
         dictionaries.append(dictionary)
-    session.bulk_insert_mappings(typ,dictionaries)
+    session.bulk_insert_mappings(typ, dictionaries)
     session.commit()
